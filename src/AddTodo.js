06396925f4b83954bb4de13784754069a721ce65
Kyle Devine <devinenoise@gmail.com>
2020-02-26 11:27:43 -0800

    functioning

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -6,7 +6,7 @@ export default class AddTodo extends Component {
     render() {
         return (
             <div>
-                <input value={this.props.todoInput} onChange={this.props.handleInput} />
+                <input value={this.props.todoInput} onChange={this.props.handleInput} required />
                 <button className="myButton" onClick={this.props.handleClick}>Add Todo</button>
             </div>
         )
