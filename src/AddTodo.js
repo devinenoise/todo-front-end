348262a26fb90692da98e3149b9214b9fbad4361
Kyle Devine <devinenoise@gmail.com>
2020-02-26 09:58:21 -0800

    some styling

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react'
+import './App.css';
+
 
 export default class AddTodo extends Component {
     render() {
         return (
             <div>
                 <input value={this.props.todoInput} onChange={this.props.handleInput} />
-                <button onClick={this.props.handleClick}>Add todo</button>
+                <button className="myButton" onClick={this.props.handleClick}>Add Todo</button>
             </div>
         )
     }
--- src/TodoApp.js
+++ src/TodoApp.js
@@ -1,62 +1,76 @@
 import React, { Component } from 'react'
 import AddTodo from './AddTodo.js';
 import request from 'superagent';
+import './App.css';
+
 
 export default class TodoApp extends Component {
+    // initial state as an empty array
     state = { todos: [] }
 
-
-    componentDidMount = async() => {
+    // getting the list of todos from the database
+    componentDidMount = async () => {
         const todos = await request.get('https://secure-river-88477.herokuapp.com/api/todos')
 
         this.setState({ todos: todos.body })
     }
 
+    // creating optimistic rendering
     handleClick = async () => {
         const newTodo = {
-            // math.random() is fine here because this is a fake todo
             id: Math.random(),
             task: this.state.todoInput,
             complete: false,
         };
 
+        // spread operator
         const newTodos = [...this.state.todos, newTodo];
 
         this.setState({ todos: newTodos });
+        // updating the database with the user todo input
         const data = await request.post('https://secure-river-88477.herokuapp.com/api/todos', {
             task: this.state.todoInput
         });
     }
 
-    handleInput = (e) => { this.setState({ todoInput: e.target.value })};
-    
+    handleInput = (e) => { this.setState({ todoInput: e.target.value }) };
+
+    handleDelete = async () => {
+        await request.delete(`https://secure-river-88477.herokuapp.com/api/todos${this.state.todo.id}`);
+    }
+
     render() {
         return (
-            <div>
-                <AddTodo 
-                todoInput={ this.state.todoInput } 
-                handleClick={ this.handleClick } 
-                handleInput={ this.handleInput } 
-            />
+            <div className="container">
+                <AddTodo
+                    todoInput={this.state.todoInput}
+                    handleClick={this.handleClick}
+                    handleInput={this.handleInput}
+                />
                 {
-                    this.state.todos.map((todo) => <p 
+                    this.state.todos.map((todo) => <p className="todos"
                         style={{
                             textDecoration: todo.complete ? 'line-through' : 'none'
                         }}
                         onClick={async () => {
-                            // lets mutate! make a copy of the array in state
-                        const newTodos = this.state.todos.slice();
-                            // go find whichever todo we're talking about here
-                        const matchingTodo = newTodos.find((thisTodo) => todo.id === thisTodo.id);
 
-                        matchingTodo.complete = !todo.complete
-                                 
-                        this.setState({ todos: newTodos });
-                        const data = await request.put(`https://secure-river-88477.herokuapp.com/api/todos/${todo.id}`, matchingTodo);
-                    }} key={todo.id}>
-                        {todo.task}
+                            // lets mutate! make a copy of the array in state
+                            const newTodos = this.state.todos.slice();
+
+                            // go find whichever todo we're talking about here
+                            const matchingTodo = newTodos.find((thisTodo) => todo.id === thisTodo.id);
+
+                            matchingTodo.complete = !todo.complete
+
+                            this.setState({ todos: newTodos });
+
+                            //updating the database actually
+                            const data = await request.put(`https://secure-river-88477.herokuapp.com/api/todos/${todo.id}`, matchingTodo);
+
+                        }} key={todo.id}> {todo.task}
                     </p>)
                 }
+
             </div>
         )
     }
